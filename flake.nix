89e9c12924695c066a5927d7cddbc31c2a1fd418
Ulrich Haboeck <ulrich@mlabs.city>
2022-10-12 12:04:37 +0200

    changed flake.nix and pdf metadata

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "FRI Survey";
+  description = "Multivariate Permutation Argument";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
@@ -9,13 +9,13 @@
     in
     {
       packages.x86_64-linux.default = pkgs.stdenvNoCC.mkDerivation {
-        name = "fri-survey";
+        name = "mv-permutation-arg";
         src = ./.;
         nativeBuildInputs = [ (pkgs.texlive.combine { inherit (pkgs.texlive) scheme-basic latexmk memoir etoolbox pdfsync lipsum mnsymbol multirow listings mdwtools xcolor framed ncctools algorithms float algorithmicx cryptocode mathtools pgf xargs xkeyval forloop pbox varwidth bigfoot environ todonotes latex-fonts lm titlesec; }) ];
         buildPhase = "latexmk -pdf";
         installPhase = ''
           mkdir $out
-          install FRI.pdf $out/"FRI Survey.pdf"
+          install FRI.pdf $out/"MVPlonk.pdf"
         '';
       };
     };
